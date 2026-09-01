48983e3852fc8a0d1e6694cded43a3158b57a8f3
reda haddan <haddan.reda.net@gmail.com>
2026-09-01 02:54:53 +0100

    feat: add short bootstrap redirect

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@ const withImages = require('next-images');
 /** @type {import('next').NextConfig} */
 module.exports = withImages({
 	reactStrictMode: true,
+	async redirects() {
+		return [
+			{
+				source: '/i',
+				destination:
+					'https://raw.githubusercontent.com/R3-da/nix-bootstrap/main/bootstrap.sh',
+				permanent: false,
+			},
+		];
+	},
 });
